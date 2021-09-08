f2d5746d5cee1ab8c2e4da3dd46156966e46cf1f
Gustavo <gu.machado.oliveira@gmail>
2021-09-08 16:39:58 -0300

    Comentando o codigo

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;;
   }
 
-const main = document.getElementsByTagName('main')[0];
-
-
-
+// CAPTURA O BOTÃO
 const button = document.querySelector('button');
 
 button.addEventListener('click', rolarDado);
 
 function rolarDado() { 
   
+  // COMPLETA O ARRAY CONTADOR COM ZEROS
   const count = [];
 
   for(let i = 0; i < 12; i++){
@@ -23,48 +21,51 @@ function rolarDado() {
 
   
   for (let i = 1; i <= 1000; i++){ 
- 
-      const result = document.createElement('p');
-      let teste = 0;
-      let teste2 = 0;
+       
+      let dado1 = 0;
+      let dado2 = 0;
       let total = 0;
    
-
-      teste = getRandomIntInclusive(1, 6);  
-      teste2 = getRandomIntInclusive(1, 6);
+      // SIMULA A ROALGEM DO PRIMEIRO DADO
+      dado1 = getRandomIntInclusive(1, 6);
       
+      // SIMULA A ROALGEM DO SEGUNDO DADO
+      dado2 = getRandomIntInclusive(1, 6);
+      
+      // SOMA O NÚMERO DA FACE DE CADA DADO
+      total = dado1 + dado2;
 
-      total = teste + teste2;
-
+      // CONTA QUANTAS VEZES A SOMA DAS FACES DOS DADOS FORAM IGUAIS
       count[total - 1] += 1      
-
-      result.innerText = total.toString();     
+         
   }
 
   for( let i = 1; i < count.length; i++){
     const imprimeTotal = document.createElement('p');
-    const barra = document.createElement('div');
+    const barra = document.createElement('div');    
 
-    const section = document.getElementById('#container');
-
+    // ADICIONA A CLASSE AO ELEMENTO
     barra.classList.add("barra");
-    // barra.style.width = count[i] + 'px';
-     barra.style.flexBasis = count[i] + 'px';
 
+    // GERA O COMPRIMENTO DA BARRA BASEADO NA QUANTIDADE DE APARIÇÕES
+    barra.style.flexBasis = count[i] + 'px';
+
+    // SELECIONA UMA DIV DIFERENTE BASEADA NO ID DA POSIÇÃO ATUAL + 1, POIS A ID INICIA EM 2
     const div = document.querySelector('#num' + (1 + i));
+
+    // APAGA OS ELEMENTOS ANTERIORES, ANTES DE APRESENTAR OS PRÓXIMOS VALORES
     div.innerText = ' ';
     
+    // ADICIONA A CLASSE AO ELEMENTO
     imprimeTotal.classList.add('p-flex');
+
+    // APENAS ESTILIZA A SAÍDA PARA O USUÁRIO E MOSTRA O RESULTADO TOTAL DAS JOGADAS
     imprimeTotal.innerText = '[' + (1 + i) + ']' +': ' + count[i];
 
     
+    // CRIA OS ELEMENTOS NA PÁGINA
     div.appendChild(imprimeTotal); 
     div.appendChild(barra);
-
-    
-
-    // main.appendChild(div);
-
   }
 
 };
